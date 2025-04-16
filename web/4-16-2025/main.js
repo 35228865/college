e3f940b992ae45a5c6585430a4e2cd20406d6c84
35228865 <ink99999@gmail.com>
2025-04-16 15:50:53 +0800

    Update main.js and add data.js

--- web/4-16-2025/main.js
+++ web/4-16-2025/main.js
@@ -1,16 +1,47 @@
-let trace1 = {
-    type: "scatter",
-    mode: "lines+markers",
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16]
-};
-
-for (let i = 0; i < set1.length; i++) {
-    trace1.x.push(set1[i][0]);
-    trace1.y.push(set1[i][1]);
+d3.csv('https://raw.githubusercontent.com/ryanchung403/dataset/refs/heads/main/harry_potter.csv').then(res =>{
+    console.log(res);
+    drawGraph(res);
+});
+//unpack the data
+function unpack(rows, key)
+{
+    return rows.map(function (row) 
+    {
+        return row[key];
+    });
 }
-let data =[trace1];
-let layout = {
-    margin:{t:50,b:30,l:30,r:20}
-};
-Plotly.newPlot("myGraph",data,layout);
+function drawGraph(res) 
+{
+    console.log(res);
+    console.log(unpack(res, "release_year"));
+    console.log(unpack(res, "revenue"));
+    console.log(unpack(res, "budget"));
+    let trace1 = {  
+        type: "scatter",
+        mode: "lines+markers",
+        name: "Revenue",
+        x: unpack(res, "release_year"),
+        y: unpack(res, "revenue")
+    };
+    let trace2 = {
+        type: "scatter",
+        mode: "lines+markers",
+        name: "Budget",
+        x: unpack(res, "release_year"),
+        y: unpack(res, "budget")
+    };
+    /*for (let i = 0; i < set1.length; i++) {
+        trace1.x.push(res[i][0]);
+        trace1.y.push(set1[i][1]);
+    }
+    for (let i = 0; i < set2.length; i++) {
+        trace2.x.push(set2[i][0]);
+        trace2.y.push(set2[i][1]);
+    }*/
+    let data =[trace1,trace2];
+    let layout = {
+        margin:{t:50,b:30,l:30,r:20},
+        title:{text:"My Graph"},
+    };
+    Plotly.newPlot("myGraph",data,layout);
+}
